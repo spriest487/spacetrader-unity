f24856c5eb947d1a0b17711e8c5e635166cb6714
spriest487 <spriest487@googlemail.com>
2016-01-04 00:17:53 +0000

    Added name lists for random crew generation

--- Assets/People/CrewManager.cs
+++ Assets/People/CrewManager.cs
@@ -3,5 +3,26 @@ using UnityEngine;
 
 public class CrewManager : MonoBehaviour
 {
-    public void Update() { }
+    [SerializeField]
+    private TextAsset forenameList;
+
+    [SerializeField]
+    private TextAsset surnameList;
+
+    private string[] forenames;
+    private string[] surnames;
+
+    private static string[] LoadNamesFromTextAsset(TextAsset asset)
+    {
+        return asset.text.Split('\n');
+    }
+
+    void Start()
+    {
+        forenames = LoadNamesFromTextAsset(forenameList);
+        surnames = LoadNamesFromTextAsset(surnameList);
+    }
+
+    private string[] Forenames { get { return forenames; } }
+    private string[] Surnames { get { return surnames; } }
 }
