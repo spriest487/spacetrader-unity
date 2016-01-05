eefdedd4cf858d4a82fd174c249a12c6344f9221
spriest487 <spriest487@googlemail.com>
2016-01-05 23:40:42 +0000

    Initial class for ship crew assignments

---
+++ Assets/Crew/ShipCrewAssignments.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+[Serializable]
+public class ShipCrewAssignments
+{
+    public CrewMember captain;
+
+    public List<CrewMember> passengers;
+}
+
