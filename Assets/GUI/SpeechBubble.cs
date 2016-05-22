827ab6f6f9c04f062b966e8b20ec565f128f8365
spriest487 <spriest487@googlemail.com>
2016-05-22 22:51:53 +0100

    Added speech bubble!

---
+++ Assets/GUI/SpeechBubble.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class SpeechBubble : MonoBehaviour
+{
+    [SerializeField]
+    private RectTransform text;
+
+    [SerializeField]
+    private RectTransform icon;
+
+    public void Update()
+    {
+        var pos = transform.position;
+        float scaleX;
+        float scaleY;
+        
+        if (pos.x > Screen.width / 2)
+        {
+            scaleX = -1;
+        }
+        else
+        {
+            scaleX = 1;
+        }
+
+        if (pos.y > Screen.height / 2)
+        {
+            scaleY = -1;
+        }
+        else
+        {
+            scaleY = 1;
+        }
+
+        var scale = new Vector3(scaleX, scaleY, 1);
+        transform.localScale = scale;
+        text.transform.localScale = scale;
+        icon.transform.localScale = scale;
+    }
+}
