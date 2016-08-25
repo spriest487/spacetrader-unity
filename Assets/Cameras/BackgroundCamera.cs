e3452785aa30764ee80a0eef6420088f30c76f3f
spriest487 <spriest487@googlemail.com>
2016-08-25 22:51:56 +0100

    Change FOV during cutscenes because it looks cool

---
+++ Assets/Cameras/BackgroundCamera.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class BackgroundCamera : MonoBehaviour
+{
+    public static BackgroundCamera Current { get; private set; }
+
+    public Camera Camera { get; private set; }
+
+    void Start()
+    {
+        Camera = GetComponent<Camera>();
+    }
+
+    void OnEnable()
+    {
+        Debug.Assert(!Current || Current == this);
+        Current = this;
+    }
+
+    void OnDisable()
+    {
+        Current = null;
+    }
+
+    void OnPreRender()
+    {
+        var mainCam = Camera.main;
+
+        Debug.Assert(mainCam != Camera);
+
+        transform.rotation = mainCam.transform.rotation;
+        Camera.fieldOfView = mainCam.fieldOfView;
+    }
+}
--- Assets/Cameras/MatchCameraRotation.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class MatchCameraRotation : MonoBehaviour
-{
-    void OnPreRender()
-    {
-        transform.rotation = Camera.main.transform.rotation;
-    }
-}
