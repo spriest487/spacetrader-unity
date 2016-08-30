fe6d374a6a7daeca80abc49bea9732db314c4611
Josh Bayley <josh.g.bayley@gmail.com>
2016-08-30 02:10:42 +0100

    fixed toggle color issues with SaveFileEntry and tweaked prefabs

--- Assets/Main Menu/LoadMenu/SaveFileEntry.cs
+++ Assets/Main Menu/LoadMenu/SaveFileEntry.cs
@@ -6,13 +6,18 @@ public class SaveFileEntry : Toggle {
     [SerializeField]
     private Text entryText;
 
-    private Sprite normalSprite;
+    private ColorBlock startColors;
+    private Sprite startSprite;
 
     protected override void Awake()
     {
         entryText = GetComponentInChildren<Text>();
+
         if (entryText == null)
             Debug.LogWarning("SaveFileEntry: missing text reference");
+
+        startColors = colors;
+        startSprite = image.sprite;
 
         base.Awake();
     }
@@ -21,19 +26,25 @@ public class SaveFileEntry : Toggle {
     {
         onValueChanged.RemoveAllListeners();
 
-        image.color = colors.normalColor;
-        image.sprite = image.sprite = normalSprite;
-
         isOn = false;
 
-        normalSprite = ((Image)targetGraphic).sprite;
+        startSprite = ((Image)targetGraphic).sprite;
         onValueChanged.AddListener(value =>
         {
+            var newColors = colors;
+
             switch (transition)
             {
-                case Transition.ColorTint: image.color = isOn ? colors.pressedColor : colors.normalColor; break;
-                case Transition.SpriteSwap: image.sprite = isOn ? spriteState.pressedSprite : normalSprite; break;
+                case Transition.ColorTint:
+                    newColors.normalColor = isOn ? colors.pressedColor : startColors.normalColor;
+                    newColors.highlightedColor = isOn ? colors.pressedColor : startColors.highlightedColor;
+                    break;
+                case Transition.SpriteSwap:
+                    image.sprite = isOn ? spriteState.pressedSprite : startSprite; // untested
+                    break;
             }
+
+            colors = newColors;
         });
     }
 
