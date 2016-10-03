2a8404425a153676d7aefa50aa3a4ae9e6042ac3
spriest487 <spriest487@googlemail.com>
2016-10-03 14:36:28 +0100

    Can't jump to current area

--- Assets/World Map/WorldMapArea.cs
+++ Assets/World Map/WorldMapArea.cs
@@ -19,6 +19,6 @@ public partial class WorldMapArea : ActionOnActivate
 
     public override bool CanBeActivatedBy(Ship activator)
     {
-        return true;
+        return SpaceTraderConfig.WorldMap.GetCurrentArea() != this;
     }
 }
