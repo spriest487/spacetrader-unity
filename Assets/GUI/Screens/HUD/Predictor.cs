72fad7f455a433e6c56f8767193985c3fbf6d087
spriest487 <spriest487@googlemail.com>
2016-10-06 01:23:06 +0100

    Use PooledList in Predictor to save allocations, and implemented the slow speed cutoff where the predictor hides itself

--- Assets/GUI/Screens/HUD/Predictor.cs
+++ Assets/GUI/Screens/HUD/Predictor.cs
@@ -13,45 +13,37 @@ public class Predictor : MonoBehaviour
     //need a ref for a bracketmanager because we steal styles from it
     [SerializeField]
     private BracketManager brackets;
-
-    /* doesn't need to be serialized as it's rebuilt if necessary */
-    private List<PredictorMarker> markers;
-
-    private void ClearReticules()
-    {
-        foreach (Transform child in transform)
-        {
-            Destroy(child.gameObject);
-        }
-
-        markers = new List<PredictorMarker>();
-    }
-
+    
+    private PooledList<PredictorMarker, Vector3> markers;
+    
     void LateUpdate()
     {
+        if (markers == null)
+        {
+            markers = new PooledList<PredictorMarker, Vector3>(transform, markerPrefab);
+        }
+
         var player = PlayerShip.LocalPlayer;
         var camera = FollowCamera.Current.Camera;
+        Ship playerShip;
 
-        if (!player || !camera)
+        if (!player 
+            || !(playerShip = player.Ship) 
+            || !player.Ship.Target 
+            || player.Ship.Target.TargetSpace != TargetSpace.Local
+            || !camera)
         {
-            ClearReticules();
+            markers.Clear();
             return;
         }
-
-        var playerShip = player.GetComponent<Ship>();
-
-        if (!playerShip || !playerShip.Target)
-        {
-            ClearReticules();
-            return;
-        }
-
+        
         var loadout = playerShip.ModuleLoadout;
         var modCount = loadout.SlotCount;
 
         var newMarkerPositions = new List<Vector3>(modCount);
+        //var markerItems = new List<HardpointModule>();
 
-        var playerTargetable = player.GetComponent<Targetable>();
+        var playerTargetable = playerShip.Targetable;
         
         /* one marker per active hardpoint module which returns true for
         IsPredictable. we get the prediction points from the module's 
@@ -64,16 +56,12 @@ public class Predictor : MonoBehaviour
                 //empty slot
                 continue;
             }
-
+            
             var behavior = module.ModuleType.Behaviour;
-
             var predictPoint = behavior.PredictTarget(playerShip, moduleIndex, playerShip.Target);
 
             if (predictPoint.HasValue)
             {
-                //PredictTarget returns world pos
-                predictPoint = camera.WorldToScreenPoint(predictPoint.Value);
-
                 if (predictPoint.HasValue && predictPoint.Value.z >= 0)
                 {
                     newMarkerPositions.Add(predictPoint.Value);
@@ -81,29 +69,29 @@ public class Predictor : MonoBehaviour
             }
         }
 
-        /* if the number of markers has changed, we need to drop all our
-        children and rebuild the marker list */
-        var markerCount = newMarkerPositions.Count;
-        if (markers == null || markers.Count != markerCount)
+        var targetOrigin = player.Ship.Target.transform.position;
+        float maxDistSqr = 0;
+        for (int pos = 0; pos < newMarkerPositions.Count; ++pos)
         {
-            ClearReticules();
-
-            for (int marker = 0; marker < newMarkerPositions.Count; ++marker)
-            {
-                var newMarker = Instantiate(markerPrefab);
-                newMarker.transform.SetParent(transform, false);
-
-                markers.Add(newMarker);
-            }
+            float distSqr = (targetOrigin - newMarkerPositions[pos]).sqrMagnitude;
+            maxDistSqr = Mathf.Max(maxDistSqr, distSqr);
         }
 
-        var predictorColor = brackets.GetBracketColor(playerTargetable, playerShip.Target);
-
-        /* set positions of the (now correctly sized) marker list */
-        for (int marker = 0; marker < markerCount; ++marker)
+        if (maxDistSqr < MIN_SPEED)
         {
-            markers[marker].transform.position = newMarkerPositions[marker];
-            markers[marker].Color = predictorColor;
+            markers.Clear();
+        }
+        else
+        {
+            var predictorColor = brackets.GetBracketColor(playerTargetable, playerShip.Target);
+            markers.Refresh(newMarkerPositions, (i, marker, position) =>
+            {
+                //PredictTarget returns world pos
+                var screenPos = camera.WorldToScreenPoint(position);
+
+                marker.transform.position = screenPos;
+                marker.Color = predictorColor;
+            });
         }
     }
 }
