f40273bb8adb8fcdce1d5d42daf955a3fdcc42c5
spriest487 <spriest487@googlemail.com>
2016-10-16 21:50:58 +0100

    Updated Fleet and Jobs screens text layouts

---
+++ Assets/GUI/MultiElementFitter.cs
@@ -0,0 +1,56 @@
+﻿#pragma warning disable 0649
+
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(LayoutElement))]
+public class MultiElementFitter : MonoBehaviour
+{
+    [SerializeField]
+    private List<RectTransform> targets;
+
+    [SerializeField]
+    private ContentSizeFitter.FitMode fitMode = ContentSizeFitter.FitMode.PreferredSize;
+
+    [SerializeField]
+    private int extraPadding = 0;
+
+    private LayoutElement layout;
+
+    private void Start()
+    {
+        layout = GetComponent<LayoutElement>();
+    }
+
+    private void Update()
+    {
+#if UNITY_EDITOR
+        if (targets == null)
+        {
+            targets = new List<RectTransform>();
+        }
+
+        Start();
+#endif
+
+        float total = 0;
+        foreach (var element in targets)
+        {
+            total += LayoutUtility.GetPreferredHeight(element);
+        }
+
+        int size = extraPadding + Mathf.CeilToInt(total);
+
+        switch (fitMode)
+        {
+            case ContentSizeFitter.FitMode.MinSize:
+                layout.minHeight = size;
+                break;
+            case ContentSizeFitter.FitMode.PreferredSize:
+                layout.preferredHeight = size;
+                break;
+        }
+    }
+}
--- Assets/GUI/Screens/QuestBoard/QuestList.cs
+++ Assets/GUI/Screens/QuestBoard/QuestList.cs
@@ -1,21 +1,33 @@
 ﻿#pragma warning disable 0649
 
-using System;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class QuestList : MonoBehaviour
 {
+    [Header("Available Quests")]
+
     [SerializeField]
     private Transform itemsRoot;
 
     [SerializeField]
-    private Transform acceptedItemsRoot;
+    private Text availableHeader;
+
+    [Header("Accepted Quests")]
+
+    [SerializeField]
+    private Text acceptedHeader;
+
+    [SerializeField]
+    private Transform acceptedRoot;
+
+    [Header("Prefabs")]
 
     [SerializeField]
     private QuestListItem itemPrefab;
 
     private PooledList<QuestListItem, Quest> questList;
-
     private PooledList<QuestListItem, Quest> acceptedQuestList;
 
     public void Close()
@@ -35,13 +47,54 @@ public class QuestList : MonoBehaviour
             questList = new PooledList<QuestListItem, Quest>(itemsRoot, itemPrefab);
         }
 
-        var station = SpaceTraderConfig.LocalPlayer.Moorable.DockedAtStation;
-        var allQuests = SpaceTraderConfig.QuestBoard.QuestsAtStation(station);
+        if (acceptedQuestList == null)
+        {
+            acceptedQuestList = new PooledList<QuestListItem, Quest>(acceptedRoot, itemPrefab);
+        }
 
-        questList.Refresh(allQuests, (index, listItem, quest) => listItem.Assign(quest));
+        var station = SpaceTraderConfig.LocalPlayer.Moorable.DockedAtStation;
+        var myQuests = SpaceTraderConfig.QuestBoard.QuestsForPlayer(PlayerShip.LocalPlayer);
+        var availableQuests = SpaceTraderConfig.QuestBoard.QuestsAtStation(station)
+            .Except(myQuests);
+
+        if (station)
+        {
+            availableHeader.gameObject.SetActive(true);
+
+            questList.Refresh(availableQuests, (index, listItem, quest) => 
+                listItem.Assign(quest));
+
+            if (questList.Count == 0)
+            {
+                availableHeader.text = "No jobs available at " + station.name;
+                itemsRoot.gameObject.SetActive(false);
+            }
+            else
+            {
+                availableHeader.text = "Jobs available at " + station.name;
+                itemsRoot.gameObject.SetActive(true);
+            }
+        }
+        else
+        {
+            questList.Clear();
+            availableHeader.gameObject.SetActive(false);
+            itemsRoot.gameObject.SetActive(false);
+        }
+
+        acceptedQuestList.Refresh(myQuests, (index, listItem, quest) =>
+            listItem.Assign(quest));
+        bool haveAcceptedQuests = acceptedQuestList.Count > 0;
+
+        acceptedHeader.gameObject.SetActive(haveAcceptedQuests);
+        acceptedRoot.gameObject.SetActive(haveAcceptedQuests);
 
         //status of quests can change independent of which quests are displayed
         foreach (var item in questList)
+        {
+            item.UpdateStatus();
+        }
+        foreach (var item in acceptedQuestList)
         {
             item.UpdateStatus();
         }
