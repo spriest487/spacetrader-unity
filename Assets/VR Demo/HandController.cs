e5191a3a1b60a11cd228a0de8d2fd4fe2344ce57
spriest487 <spriest487@googlemail.com>
2017-03-16 22:35:31 +0000

    VR demo scene initial commit

---
+++ Assets/VR Demo/HandController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+
+public class HandController : MonoBehaviour
+{
+    [SerializeField]
+    private VRNode node;
+    
+	private void Update ()
+    {
+        var pos = InputTracking.GetLocalPosition(node);
+        var rot = InputTracking.GetLocalRotation(node);
+
+        transform.position = pos;
+        transform.rotation = rot;
+	}
+}
