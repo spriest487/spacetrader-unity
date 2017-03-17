b920be963fc3c734f0862aa761522fa8f950cbb9
spriest487 <spriest487@googlemail.com>
2017-03-17 02:15:35 +0000

    Made a new material for the move order line

--- Assets/VR Demo/HandController.cs
+++ Assets/VR Demo/HandController.cs
@@ -67,8 +67,6 @@ public class HandController : MonoBehaviour
             moveLine.SetPosition(1, to);
 
             var dist = (from - to).magnitude;
-            moveLine.material.mainTextureScale = new Vector2(dist, 1);
-            moveLine.material.renderQueue = 3001;
 
             yield return null;
         }
